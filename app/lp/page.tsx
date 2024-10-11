c46da92ef73b17299779ab1e7793688d66291c1c
RyogoIimura <116061207+RyogoIimura@users.noreply.github.com>
2024-10-11 20:33:08 +0900

    users post 途中

--- app/lp/page.tsx
+++ app/lp/page.tsx
@@ -1,6 +1,26 @@
 "use client";
+import { API_URL } from "@/constants/url";
+import { useSession } from "next-auth/react"
 
 export default function Lp() {
+  const { data: session } = useSession();
+
+  console.log(session);
+  if(session){
+    const response = await fetch(`${API_URL}/createUsers`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: session.name,
+        email: session.email,
+      }),
+    });
+    if (response.ok) {
+      const newItem = await response.json();
+      mutate([...items, JSON.parse(newItem)]);
+    }
+  }
+
   return (
     <>
     </>
