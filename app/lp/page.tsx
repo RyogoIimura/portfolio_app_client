3275aeb0212a7fadec6b6cb67bac296e69f07f5b
RyogoIimura <116061207+RyogoIimura@users.noreply.github.com>
2024-10-13 16:30:39 +0900

    ログイン後 users テーブル作成

--- app/lp/page.tsx
+++ app/lp/page.tsx
@@ -1,6 +1,44 @@
 "use client";
+import { useSession } from "next-auth/react"
+import { useEffect } from "react";
+
+import { API_URL } from "@/constants/url";
+import { useUsers } from "../../hooks/useUsers";
+import { userType } from "@/types/types";
 
 export default function Lp() {
+  const { data: session } = useSession();
+  const { users, mutate } = useUsers();
+
+  const handleCreateUser = async () => {
+    const response = await fetch(`${API_URL}/createUser`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: session?.user?.name,
+        email: session?.user?.email,
+      }),
+    });
+
+    if (response.ok) {
+      const newUser = await response.json();
+      mutate([...users, newUser]);
+    }
+  };
+  
+  useEffect(() => {
+    let createFlag = true;
+    users?.map((user: userType) => {
+      if( session?.user?.email === user.email ) createFlag = false;
+    })
+    if( createFlag === true ){
+      handleCreateUser();
+      console.log(`create`);
+    } else {
+      console.log(`false`);
+    }
+  }, [session])
+
   return (
     <>
     </>
--- types/types.ts
+++ types/types.ts
@@ -1,3 +1,14 @@
+export type userType = {
+  id: string;
+  name: string;
+  email: string;
+  tel?: string;
+  address?: string;
+  birth?: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
 export type ItemType = {
   id: string;
   name: string;
