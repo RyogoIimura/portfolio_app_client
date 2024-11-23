61280d5cd916761e89078233adc4e50b2f711536
RyogoIimura <116061207+RyogoIimura@users.noreply.github.com>
2024-11-23 23:56:19 +0900

    リダイレクトコメントアウト

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -3,16 +3,16 @@ import { ItemType } from "@/types/types";
 import EditForm from '../components/manage/EditForm'
 import CreateForm from '../components/manage/CreateForm'
 import { useItems } from "../../hooks/useItems";
-import { useSession } from "next-auth/react";
-import { MANAGE_ID } from "@/constants/url";
+// import { useSession } from "next-auth/react";
+// import { MANAGE_ID } from "@/constants/url";
 import Header from "../components/Header";
 
 
 export default function Manage() {
-  const { data: session } = useSession();
+  // const { data: session } = useSession();
   const { items } = useItems();
 
-  if( session === undefined || session?.user?.id != MANAGE_ID ) document.location = '../lp';
+  // if( session === undefined || session?.user?.id != MANAGE_ID ) document.location = '../lp';
 
   return (
     <>
